a0945253e353452058e76226a8040ec84c4de6ff
Colum <colum.mcgaley@fastmail.com>
2021-02-12 13:01:59 -0800

    It works!

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,33 +1,35 @@
 use std::sync::Arc;
-use zookeeper::{ZooKeeper, Watcher, WatchedEventType, WatchedEvent};
+use zookeeper::{ZooKeeper};
 use anyhow::{Result, Context};
 use uuid::Uuid;
 use log::{debug, trace, warn};
 use std::sync::mpsc::{SyncSender, Receiver, sync_channel};
-use lru;
-
-
-/// Prefix to use in /<dir>/queue/<prefix>000000001
-const ZK_DISTRIBUTEDQUEUE_PREFIX: &str = "zkmq-";
+use crate::producer::ZK_DISTRIBUTEDQUEUE_PREFIX;
+use crate::{ZkMQMessage, ZkPath, ZkMQMessageMetadata};
+use std::time::Instant;
 
 /// Number of children to check when we're filtering the results.
 const ZKMQ_CONSUMER_CONSUMPTION_LIMIT: usize = 12;
 
-const ZKMQ_TASK_GC_SWEEP_INTERVAL: u16 = 600000; // 10 minutes in ms
-const ZKMQ_DEFAULT_TASK_GC_INTERVAL: u16 = 28800000;  // 8 hours in ms
+// const ZKMQ_TASK_GC_SWEEP_INTERVAL: u32 = 600000; // 10 minutes in ms
+// const ZKMQ_DEFAULT_TASK_GC_INTERVAL: u32 = 28800000;  // 8 hours in ms
 
 pub enum FilterValue {
     String(String),
     // Boolean(bool),
     Integer(i32),
+    OrderedEnum(String, Vec<String>)
 }
 
 pub enum FilterOperator {
-    Equal,
-    GreaterThan,
-    LessThan
+    Eq,  // ==
+    Gt,  // >
+    Lt,  // <
+    Ge,  // >=
+    Le,  // <=
 }
 
+#[derive(PartialEq)]
 pub enum FilterConditional {
     ALL,
     ANY
@@ -39,8 +41,8 @@ pub struct Filter {
     pub operator: FilterOperator
 }
 impl Filter {
-    pub fn check_match(&self, o: &mut ZkMQConsumer, task: &String) -> Result<bool> {
-        let path = format!("{}/tasks/{}", o.dir, task);
+    pub fn check_match(&self, o: &mut ZkMQConsumer, task: &str) -> Result<bool> {
+        let path = format!("{}/task/{}", o.dir, task);
         // loop up the key in the lru cache- because we might have seen it before
         let value = match o.cache.get(&path) {
             None => match o.zk.get_data(path.as_str(), false) {
@@ -52,12 +54,14 @@ impl Filter {
         };
 
         Ok(match &self.value {
-            FilterValue::String(s) => {
+            FilterValue::String(v) => {
                 let d = String::from_utf8(value).context("parsing filter field to string")?;
                 match &self.operator {
-                    FilterOperator::Equal => &d == s,
-                    FilterOperator::GreaterThan => &d > s,
-                    FilterOperator::LessThan => &d < s,
+                    FilterOperator::Eq => &d == v,
+                    FilterOperator::Gt => &d > v,
+                    FilterOperator::Ge => &d >= v,
+                    FilterOperator::Lt => &d < v,
+                    FilterOperator::Le => &d <= v,
                 }
             },
             // FilterValue::Boolean(b) => {
@@ -68,14 +72,34 @@ impl Filter {
             //         FilterOperator::LessThan => d < s,
             //     }
             // },
-            FilterValue::Integer(i) => {
-                let d: i32 = i32::from_ne_bytes(value.as_slice()).context("parsing filter field to i32")?;
+            FilterValue::Integer(v) => {
+                let d: i32 = String::from_utf8(value).context("parsing filter field to i32")?.parse()?;
                 match &self.operator {
-                    FilterOperator::Equal => &d == i,
-                    FilterOperator::GreaterThan => &d > i,
-                    FilterOperator::LessThan => &d < i,
+                    FilterOperator::Eq => &d == v,
+                    FilterOperator::Gt => &d > v,
+                    FilterOperator::Ge => &d >= v,
+                    FilterOperator::Lt => &d < v,
+                    FilterOperator::Le => &d <= v,
                 }
             },
+            FilterValue::OrderedEnum(key, values) => {
+                let tgt = String::from_utf8(value).context("parsing filter field to string")?;
+                let key_index = values.iter().position(|k| k == key);
+                let target_index = values.iter().position(|k| k == &tgt);
+
+                if key_index.is_none() || target_index.is_none() { false }
+                else {
+                    let d = key_index.unwrap();
+                    let v = target_index.unwrap();
+                    match &self.operator {
+                        FilterOperator::Eq => d == v,
+                        FilterOperator::Gt => d > v,
+                        FilterOperator::Ge => d >= v,
+                        FilterOperator::Lt => d < v,
+                        FilterOperator::Le => d <= v,
+                    }
+                }
+            }
         })
     }
 }
@@ -95,22 +119,22 @@ pub struct ZkMQConsumer {
 }
 
 impl ZkMQConsumer {
-    pub fn new(zk: Arc<ZooKeeper>, dir: String, id: Option<String>) -> Result<Self> {
+    pub fn new(zk: Arc<ZooKeeper>, dir: &str, id: Option<&str>) -> Result<Self> {
         let acl = zookeeper::Acl::open_unsafe().clone();
         let id = match id.is_some() {
-            true  => id.unwrap(),
-            false => Uuid::new_v4().into()
+            true  => id.unwrap().to_string(),
+            false => Uuid::new_v4().to_string()
         };
         debug!("setting instance id to {}", &id);
 
-        if !zk.exists(&dir, false)? {
+        if zk.exists(&dir, false)?.is_none() {
             zk.create(&dir, vec![], acl.clone(), zookeeper::CreateMode::Container)?;
         }
 
-        for znode in vec!["queue", "tasks", "conf", "lock"] {
-            if !zk.exists(format!("{}/{}", &dir, &znode).into(), false)? {
+        for znode in &["queue", "task", "conf", "lock"] {
+            if zk.exists(format!("{}/{}", &dir, &znode).as_str(), false)?.is_none(){
                 trace!("{}/{} does not exist. creating", &dir, &znode);
-                zk.create(format!("{}/{}", &dir, &znode).into(), vec![], acl.clone(), zookeeper::CreateMode::Container)?;
+                zk.create(format!("{}/{}", &dir, &znode).as_str(), vec![], acl.clone(), zookeeper::CreateMode::Persistent)?;
             } else {
                 trace!("{}/{} already exists", &dir, &znode);
             }
@@ -118,7 +142,7 @@ impl ZkMQConsumer {
 
         let cache = lru::LruCache::new(1024);
 
-        Ok(Self { zk, dir, id, acl , cache})
+        Ok(Self { zk, dir: dir.to_string(), id, acl , cache})
     }
 
     fn claim(&self, key: String) -> zookeeper::ZkResult<Vec<u8>> {
@@ -131,7 +155,7 @@ impl ZkMQConsumer {
     fn ordered_children<W: zookeeper::Watcher + 'static>(&self, watcher: Option<W>) -> Result<Vec<String>> {
         let mut children: Vec<(u64, String)> = Vec::new();
         match watcher {
-            Some(w) => self.zk.get_children_w(&self.dir, w),
+            Some(w) => self.zk.get_children_w(&*format!("{}/queue", &self.dir), w),
             None => self.zk.get_children(&self.dir, false) // false I think?
         }?.iter().for_each(|child| {
             // the child names will be like qn-0000001. chop off the prefix, and try and convert the
@@ -150,13 +174,13 @@ impl ZkMQConsumer {
     /// Filter Children.
     ///
     /// Process is to iterate over each given child, then iterate over each filter to check:
-    /// 1. if the filter field exists (/<dir>/tasks/<id>/filters/<filter>
+    /// 1. if the filter field exists (/<dir>/tasks/<id>/filters/<filter>)
     /// 2. if the filter matches the conditional
     /// Once all filters have been checked, evaluate the conditional. If the filer group is using
     /// FilterConditional::ALL, all filters need to match. If FilterConditional::ANY, only a single
     /// filter needs to match
-    fn filter_children(&self, children: Vec<String>, filters: Filters, limit: usize) -> Result<Vec<String>> {
-        if children.is_empty() || filters.is_empty() {
+    fn filter_children(&mut self, children: Vec<String>, filters: &Filters, limit: usize) -> Result<Vec<String>> {
+        if children.is_empty() || filters.filters.is_empty() {
             trace!("filter_children was given 0 children or 0 filters, nothing to do");
             return Ok(children);
         }
@@ -167,16 +191,18 @@ impl ZkMQConsumer {
 
             // TODO can we make this parallel? that might require support in the zookeeper library
             // TODO there should be a more logical way to do this
-            let filter_results: Vec<bool> = filters.filters.iter().map(|f| f.check_match(&self, &child)).collect();
+            let mut filter_results: Vec<bool> = vec![];
+            for f in &filters.filters {
+                filter_results.push(f.check_match(self, &child)?);
+            }
             let mut valid = 0;
-            for fr in filter_results {
-                if fr { valid += 1; }
+            for fr in &filter_results {
+                if *fr { valid += 1; }
             }
 
             // if we're doing AND, and we have as many valid filters as filters- we can say this child matches
-            if filters.conditional == FilterConditional::ALL && filter_results.len() == valid {
-                valid_children.push(child);
-            } else if filters.conditional == FilterConditional::ANY && !filter_results.is_empty() && valid > 0 {
+            if (filters.conditional == FilterConditional::ALL && filter_results.len() == valid) ||
+               (filters.conditional == FilterConditional::ANY && !filter_results.is_empty() && valid > 0) {
                 valid_children.push(child);
             }
 
@@ -188,17 +214,55 @@ impl ZkMQConsumer {
         Ok(valid_children)
     }
 
-    pub fn consume(&self, constraints: Option<Filters>) -> zookeeper::ZkResult<Vec<u8>> {
+    fn get_child(&mut self, path: ZkPath) -> Result<Vec<u8>> {
+        if let Some(data) = self.cache.get(&path.to_string()) {
+            Ok(data.clone())
+        } else {
+            // we don't have the key in the cache, so get it, put it in the cache, and return it
+            let rq = self.zk.get_data(&path.to_string(), false).context(format!("looking up {}", &path))?.0;
+            let _ = self.cache.put(path.to_string(), rq.clone());
+            Ok(rq)
+        }
+    }
+
+    fn build_message(&mut self, raw_claim: Vec<u8>) -> Result<ZkMQMessage> {
+        let claim = String::from_utf8(raw_claim).context("parsing raw claim data")?;
+        let dir = ZkPath::new(&self.dir).join("task").join(claim);
+        trace!("building ZkMQMessage from {}", &dir);
+
+        let metadata: Option<ZkMQMessageMetadata> = match self.zk.get_data(&dir.join("metadata").to_string(), false) {
+            Ok(d) => Some(serde_json::from_slice(&*d.0).context("reading task metadata")?),
+            _ => None
+        };
+
+        let id = dir.parts.last().unwrap();
+        let mut filters: Vec<(String, Vec<u8>)> = vec![];
+
+        for child in self.zk.get_children(&dir.join("filters").to_string(), false).context(format!("getting children of {}", &dir.join("filters")))? {
+            filters.push((child.clone(), self.get_child(dir.join("filters").join(child))?))
+        }
+
+        Ok(ZkMQMessage {
+            id: id.clone(),
+            filters,
+            body: self.zk.get_data(&dir.join("metadata").to_string(), false).context("reading task body")?.0,
+            meta: metadata
+        })
+
+    }
+
+    pub fn consume(&mut self, constraints: Option<Filters>) -> Result<ZkMQMessage> {
         let latch: (SyncSender<bool>, Receiver<bool>) = sync_channel(1);
         loop {
+            let start = Instant::now();
             let tx = latch.0.clone();
             let op = self.ordered_children(Some(move |ev| {
                 handle_znode_change(&tx, ev)
             }))?;
 
-            let children = match constraints.is_some() {
-                true => self.filter_children(op, constraints.unwrap(), ZKMQ_CONSUMER_CONSUMPTION_LIMIT),
-                false => op
+            let children = match constraints {
+                Some(ref inner) => self.filter_children(op, inner, ZKMQ_CONSUMER_CONSUMPTION_LIMIT)?,
+                None => op
             };
 
             if !children.is_empty()  {
@@ -209,12 +273,16 @@ impl ZkMQConsumer {
                     false => children.len()
                 };
 
-                for i in 0..limit {
-                    return match self.claim(format!("{}/queue/{}", self.dir, children[i])) {
+                for child in children.iter().take(limit) {
+                    return match self.claim(format!("{}/queue/{}", self.dir, child)) {
                         // someone else has claimed this task already, so try again
                         Err(e) if e == zookeeper::ZkError::NoNode => continue,
-                        Err(e) => Err(e),
-                        Ok(claim) => Ok(claim)
+                        Err(e) => Err(e.into()),
+                        Ok(claim) => {
+                            let inner = self.build_message(claim)?;
+                            debug!("consumed message {} ({}). took {}ms", &inner.id, &child, start.elapsed().as_millis());
+                            Ok(inner)
+                        }
                     };
                 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,62 @@
+use std::fmt;
+use serde::{Deserialize, Serialize};
+use chrono::prelude::{DateTime, Utc};
+use zookeeper::ZooKeeper;
+use std::sync::Arc;
+use anyhow::{Result, Context};
+
 pub mod consumer;
 pub mod producer;
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ZkMQMessageMetadata {
+    insert_time: DateTime<Utc>,
+    src_node: String
+}
 
-// defaults
+#[derive(Debug)]
+pub struct ZkMQMessage {
+    id: String,
+    filters: Vec<(String, Vec<u8>)>,
+    body: Vec<u8>,
+    meta: Option<ZkMQMessageMetadata>,
+}
+impl ZkMQMessage {
+    fn new<B: Into<Vec<u8>>>(body: B) -> Self {
+        ZkMQMessage {
+            id: uuid::Uuid::new_v4().to_string(),
+            filters: vec![],
+            body: body.into(),
+            meta: None
+        }
+    }
+
+    pub fn set_filter<S: ToString, B: Into<Vec<u8>>>(&mut self, key: S, value: B) {
+        self.filters.push((key.to_string(), value.into()))
+    }
+
+}
+
+pub struct ZkPath {
+    parts: Vec<String>
+}
+impl ZkPath {
+    fn new<R: ToString>(base: R) -> Self {
+        Self { parts: base.to_string().split('/').map(|s| s.to_string()).collect() }
+    }
+    fn join<D: ToString>(&self, dir: D) -> Self {
+        let mut parts = self.parts.clone();
+        parts.push(dir.to_string());
+        ZkPath {
+            parts
+        }
+    }
+}
+impl fmt::Display for ZkPath {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.parts.join("/"))
+    }
+}
 
 
 #[cfg(test)]
